11ff9a7a371cf507af56eb4eb5ea518ec207e11a
sara369 <47646689+sara369@users.noreply.github.com>
2022-12-10 21:53:36 -0800

    Add files via upload

--- canvas practise/main.js
+++ canvas practise/main.js
@@ -7,17 +7,77 @@ let theCanvas = document.getElementById("our-canvas"),
     h = theCanvas.clientHeight;
 
 
-//draw chart column
+//begin the path
+theContext.beginPath();
+
+//Select the start position
+theContext.moveTo(0,0);
+
+//Select the path points
+
+theContext.lineTo(w/2, 100);
+
+//Adjust style and line width
+
+theContext.strokeStyle = "#00f";
+theContext.lineWidth = 5;
+
+//Draw the path
+
+theContext.stroke();
+
+//begin the path
+theContext.beginPath();
+
+//Select the start position
+theContext.moveTo(w,0);
+
+//Select the path points
+
+theContext.lineTo(w/2, 100);
+
+//Adjust style and line width
+
+theContext.strokeStyle = "#00f";
+theContext.lineWidth = 5;
+
+//Draw the path
+
+theContext.stroke();
 
 
-theContext.fillStyle = "#d35400";
-theContext.fillRect(50, 50, 60, h-50)
 
-theContext.fillStyle = "#16a085";
-theContext.fillRect(150, 100, 60, h-100)
 
-theContext.fillStyle = "#8e44ad";
-theContext.fillRect(250, 80, 60, h-80)
+
+
+
+
+
+
+
+
+
+
+
+//Set the font
+// theContext.font = '30px Tahoma';
+
+//raw chart columns
+
+// theContext.fillStyle = "#d35400";
+// theContext.fillRect(50, 50, 60, h-50)
+// theContext.fillStyle = "#fff";
+// theContext.fillText("1", 70, h-15)
+
+// theContext.fillStyle = "#16a085";
+// theContext.fillRect(150, 100, 60, h-100)
+// theContext.fillStyle = "#fff";
+// theContext.fillText("2", 170, h-15)
+
+// theContext.fillStyle = "#8e44ad";
+// theContext.fillRect(250, 80, 60, h-80)
+// theContext.fillStyle = "#fff";
+// theContext.fillText("3", 270, h-15)
 
 
 
